6c783f6a59c8bf7d271a7b49b0c45466c7d347c4
Henil15 <75368846+Henil15@users.noreply.github.com>
2022-06-02 12:45:21 +0530

    Add files via upload

---
+++ class 97 python.py
@@ -0,0 +1,27 @@
+myname = "henil"
+print(myname)
+
+myage = "15"
+print(myage)
+
+myfriendslist = ["raghu" , "ram" , "mithil", "preet"]
+print (myfriendslist[1])
+
+pocketmoney =int(input("enter the pocket money you get: "))
+print (pocketmoney)
+if(pocketmoney>500):
+    print("you are a rich kid")
+elif(pocketmoney>100):
+    print("you have a good life")
+else:
+    print("i know what you feel")
+
+myfriendslist = ["raghu" , "ram" , "mithil", "preet"]
+for friend in myfriendslist:
+    print(friend)
+
+count = 5
+while(count>=0):
+    print(count)
+    count=count-1
+
